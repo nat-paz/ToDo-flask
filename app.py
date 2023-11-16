c40af1c3217b6bb0a9c57b60805509d7835a7e72
nat-paz <nataliapazsa@gmail.com>
2023-11-16 10:50:58 -0300

    up

--- app.py
+++ app.py
@@ -31,15 +31,26 @@ def addTarefas():
 
 @app.route('/update/<string:tarefa_antiga>/<string:tarefa_nova>', methods=['PUT'])
 def update_user(tarefa_antiga, tarefa_nova):
-    item = request.json
     tarefas = pd.read_csv('Text.csv')
-    tarefas = tarefas.to_dict('records') 
 
-    for dicionario in tarefas:
-        if dicionario["Tarefa"] == tarefa_antiga:
-            dicionario["Tarefa"] = tarefa_nova
-            return "Tarefa alterada"
+    for index, row in tarefas.iterrows():
+        if row['TAREFA'] == tarefa_antiga:
+            tarefas.at[index, 'TAREFA'] = tarefa_nova
+            tarefas.to_csv('Text.csv', index=False)
+            return "Tarefa alterada com sucesso"
+        
     return "Tarefa não encontrada"
+
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    tarefas = pd.read_csv('Text.csv')
+
+    if id in tarefas['ID'].values:
+        tarefas = tarefas[tarefas['ID'] != id]
+        tarefas['ID'] = range(1, len(tarefas) + 1)
+        tarefas.to_csv('Text.csv', index=False)
+        return "Tarefa excluida com sucesso"
+    return "Id não encontrada"
 
 
 if __name__ == '__main__':
