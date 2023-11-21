e528d42fd5250757be4358aa7bb0a1ee7969b4d2
nat-paz <nataliapazsa@gmail.com>
2023-11-21 15:40:11 -0300

    up

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const tabela = document.querySelector(".tabela-js");
+const from = document.querySelector(".form-js")
+const formExc = document.querySelector(".formExc-js")
 
 axios.get('http://127.0.0.1:5000/list').then((response) => {
         getData(response.data);
@@ -14,12 +16,54 @@ axios.get('http://127.0.0.1:5000/list').then((response) => {
             <tr>
                 <th scope="row">${item.ID}</th>
                 <td>${item.TAREFA}</td>
-                <td><button class="btn bg-white" type="button" data-bs-toggle="modal" data-bs-target="#modalDel"><span class="material-symbols-outlined text-danger">
+                <td><span class="material-symbols-outlined text-danger" type="button" data-bs-toggle="modal" data-bs-target="#modalDel" onclick="excluir(${item.ID})">
                 delete
-                </span></button> <button class="btn bg-white" type="button" data-bs-toggle="modal" data-bs-target="#modalEdit"><span class="material-symbols-outlined text-success">
+                </span><span class="material-symbols-outlined text-success" type="button" data-bs-toggle="modal" data-bs-target="#modalEdit">
                 edit
-                </span></button></td>
+                </span></td>
             </tr>
             `
+        })
+    }
+
+    function add() {
+        const tarefaN = document.getElementById("tarefaN").value;
+
+        if(tarefaN.trim() !== ""){
+            
+    
+            const Tarefa = {
+                Tarefa: tarefaN,
+            };
+        
+            axios.post('http://127.0.0.1:5000/add', Tarefa)
+                .then(response => {
+                    console.log('Resposta do servidor:', response.data.Tarefa);
+                })
+                .catch(error => {
+                    console.error('Erro na requisição:', error);
+                });
+        }
+        else{
+            alert("Insira uma tarefa valida")
+        }
+    }
+
+    from.addEventListener("submit", function (event) {
+        event.preventDefault(); 
+        add();
+    });
+
+    function excluir(id) {
+        const btn = document.getElementById("btnS");
+
+        btn.addEventListener("click", function () {
+            axios.delete(`http://127.0.0.1:5000/delete/${id}`)
+                .then((response) => {
+                    console.log('Resposta do servidor:', response.data);
+                })
+                .catch((error) => {
+                    console.error('Erro na requisição DELETE:', error);
+                });
         })
     }
